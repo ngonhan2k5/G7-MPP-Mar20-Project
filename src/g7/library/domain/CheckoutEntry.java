f66784615a7263ccce14067db5d0c48bf033e785
The Huynh Dang <dangthehuynh@gmail.com>
2020-03-07 00:16:25 -0600

    checkout record

--- src/g7/library/domain/CheckoutEntry.java
+++ src/g7/library/domain/CheckoutEntry.java
@@ -5,22 +5,22 @@ import java.util.Date;
 
 public final class CheckoutEntry implements Serializable {
 	private static final long serialVersionUID = 1L;
-	private final Book book;
+	private final BookCopy book;
 	private final Date checkoutDate;
 	private final Date returnDueDate;
 	
-	private CheckoutEntry(Book book, Date checkoutDate, Date returnDueDate) {
+	private CheckoutEntry(BookCopy book, Date checkoutDate, Date returnDueDate) {
 		this.book = book;
 		this.checkoutDate = checkoutDate;
 		this.returnDueDate = returnDueDate;
 	}
 	
-	public static CheckoutEntry newCheckoutEntry(Book book, Date checkoutDate, Date returnDueDate) {
+	public static CheckoutEntry newCheckoutEntry(BookCopy book, Date checkoutDate, Date returnDueDate) {
 		if(book == null) throw new IllegalArgumentException("Not Allowed to create CheckoutEntry.");
 		return new CheckoutEntry(book, checkoutDate, returnDueDate);
 	}
 	
-	public Book getBook() {
+	public BookCopy getBook() {
 		return book;
 	}
 

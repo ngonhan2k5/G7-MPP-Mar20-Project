451918cac8f22fc843d3ae668ae3bb3a099b0a6b
The Huynh Dang <dangthehuynh@gmail.com>
2020-03-06 22:00:38 -0600

    search...

--- src/g7/library/frontcontroller/LibraryController.java
+++ src/g7/library/frontcontroller/LibraryController.java
@@ -7,11 +7,15 @@ import g7.library.domain.Book;
 import g7.library.domain.BookCopy;
 import g7.library.domain.LoginCredentials;
 import g7.library.domain.SystemUser;
+import g7.library.model.BookSearchCriteria;
+import g7.library.service.AdvancedSearchInterface;
 import g7.library.service.LibraryServiceInterface;
+import g7.library.service.impl.AdvancedSearchImpl;
 import g7.library.service.impl.LibraryServiceImpl;
 
 public class LibraryController {
 	private LibraryServiceInterface libraryService = new LibraryServiceImpl();
+	private AdvancedSearchInterface searchService = new AdvancedSearchImpl();
 
 	public Book findBookByISBN(String isbn) {
 		return libraryService.searchBookByIBSN(isbn);
@@ -28,4 +32,8 @@ public class LibraryController {
 	public SystemUser login(String loginUserName, String password) {
 		return libraryService.login(new LoginCredentials(loginUserName, password));
 	}	
+	
+	public Set<Book> searchBook(String searchString) {
+		return searchService.searchBook(new BookSearchCriteria().withSearchString(searchString));
+	}
 }
--- src/g7/library/service/AdvancedSearchInterface.java
+++ src/g7/library/service/AdvancedSearchInterface.java
@@ -1,6 +1,6 @@
 package g7.library.service;
 
-import java.util.List;
+import java.util.Set;
 
 import g7.library.domain.Book;
 import g7.library.domain.SystemUser;
@@ -8,6 +8,6 @@ import g7.library.model.BookSearchCriteria;
 import g7.library.model.SearchUserCriteria;
 
 public interface AdvancedSearchInterface {
-	public List<SystemUser> findUser(SearchUserCriteria criteria);
-	public List<Book> searchBook(BookSearchCriteria criteria);
+	public Set<SystemUser> findUser(SearchUserCriteria criteria);
+	public Set<Book> searchBook(BookSearchCriteria criteria);
 }
